2591b4846bdaf0ebda423522667b962914ce0339
bmartynov <boriska91m@gmail.com>
2022-05-04 22:39:56 +0300

    fixed tlp config: added gpu_min_freq

--- modules/hardware/thinkpad/tlp.nix
+++ modules/hardware/thinkpad/tlp.nix
@@ -57,6 +57,7 @@
       PCIE_ASPM_ON_BAT = "default";
 
       INTEL_GPU_MAX_FREQ_ON_BAT = 450;
+      INTEL_GPU_MIN_FREQ_ON_BAT = 350;
 
       WIFI_PWR_ON_AC = "off";
       WIFI_PWR_ON_BAT = "on";
